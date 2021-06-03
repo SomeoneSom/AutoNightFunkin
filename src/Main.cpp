aba6fc0d303751f6ef081c5783d5933639e10b34
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2021-06-03 06:58:50 -0700

    more things and stuff

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,6 +12,7 @@ double offset = 0;
 std::string filename = "";
 bool started = false;
 bool areWeDoneYet = false;
+TheActualBot::MiniBotsManager * manager = nullptr;
 int nowBeDone() {
     areWeDoneYet = true;
     return 0;
@@ -19,18 +20,31 @@ int nowBeDone() {
 void displayOptions() {
     std::cout << "F1: Enter Song Path (" << filename << ")" << std::endl;
     std::cout << "F2: Offset + 0.1 (" << offset << " ms)" << std::endl;
-    std::cout << "F2: Offset - 0.1 (" << offset << " ms)" << std::endl;
+    std::cout << "F3: Offset - 0.1 (" << offset << " ms)" << std::endl;
     std::cout << "F4: Start/Stop" << std::endl;
     std::cout << "Q: Quit" << std::endl;
 }
 int offsetUp() {
-    offset += 0.1;
-    displayOptions();
+    if (!started) {
+        offset += 0.1;
+        if (offset == 2.77556e-17) {
+            offset = 0;
+        }
+        displayOptions();
+    }
     return 0;
 }
 int offsetDown() {
-    offset -= 0.1;
-    displayOptions();
+    if (!started) {
+        offset -= 0.1;
+        if (offset == 2.77556e-17) {
+            offset = 0;
+        }
+        displayOptions();
+    }
+    return 0;
+}
+int setFile() {
     return 0;
 }
 int main() {
@@ -38,6 +52,7 @@ int main() {
     timeGetDevCaps(&veryImportantTimerThing, sizeof(TIMECAPS));
     timeBeginPeriod(veryImportantTimerThing.wPeriodMin);
     displayOptions();
+    HotkeyManager::allTheCallbacks.push_back({setFile, 0x70});
     HotkeyManager::allTheCallbacks.push_back({offsetUp, 0x71});
     HotkeyManager::allTheCallbacks.push_back({offsetDown, 0x72});
     HotkeyManager::allTheCallbacks.push_back({nowBeDone, 0x51});
